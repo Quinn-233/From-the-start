6046a4b38d1099954ca9cd7c74a903766af3aa7d
Lee <1435818140@qq.com>
2024-06-11 10:22:35 +0800

    1

--- HC32F005/HC32F005-Exemple/example/bt/bt_timer-1/source/main.c
+++ HC32F005/HC32F005-Exemple/example/bt/bt_timer-1/source/main.c
@@ -1,88 +1,12 @@
-/******************************************************************************
-* Copyright (C) 2016, Huada Semiconductor Co.,Ltd All rights reserved.
-*
-* This software is owned and published by:
-* Huada Semiconductor Co.,Ltd ("HDSC").
-*
-* BY DOWNLOADING, INSTALLING OR USING THIS SOFTWARE, YOU AGREE TO BE BOUND
-* BY ALL THE TERMS AND CONDITIONS OF THIS AGREEMENT.
-*
-* This software contains source code for use with HDSC
-* components. This software is licensed by HDSC to be adapted only
-* for use in systems utilizing HDSC components. HDSC shall not be
-* responsible for misuse or illegal use of this software for devices not
-* supported herein. HDSC is providing this software "AS IS" and will
-* not be responsible for issues arising from incorrect user implementation
-* of the software.
-*
-* Disclaimer:
-* HDSC MAKES NO WARRANTY, EXPRESS OR IMPLIED, ARISING BY LAW OR OTHERWISE,
-* REGARDING THE SOFTWARE (INCLUDING ANY ACOOMPANYING WRITTEN MATERIALS),
-* ITS PERFORMANCE OR SUITABILITY FOR YOUR INTENDED USE, INCLUDING,
-* WITHOUT LIMITATION, THE IMPLIED WARRANTY OF MERCHANTABILITY, THE IMPLIED
-* WARRANTY OF FITNESS FOR A PARTICULAR PURPOSE OR USE, AND THE IMPLIED
-* WARRANTY OF NONINFRINGEMENT.
-* HDSC SHALL HAVE NO LIABILITY (WHETHER IN CONTRACT, WARRANTY, TORT,
-* NEGLIGENCE OR OTHERWISE) FOR ANY DAMAGES WHATSOEVER (INCLUDING, WITHOUT
-* LIMITATION, DAMAGES FOR LOSS OF BUSINESS PROFITS, BUSINESS INTERRUPTION,
-* LOSS OF BUSINESS INFORMATION, OR OTHER PECUNIARY LOSS) ARISING FROM USE OR
-* INABILITY TO USE THE SOFTWARE, INCLUDING, WITHOUT LIMITATION, ANY DIRECT,
-* INDIRECT, INCIDENTAL, SPECIAL OR CONSEQUENTIAL DAMAGES OR LOSS OF DATA,
-* SAVINGS OR PROFITS,
-* EVEN IF Disclaimer HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
-* YOU ASSUME ALL RESPONSIBILITIES FOR SELECTION OF THE SOFTWARE TO ACHIEVE YOUR
-* INTENDED RESULTS, AND FOR THE INSTALLATION OF, USE OF, AND RESULTS OBTAINED
-* FROM, THE SOFTWARE.
-*
-* This software may be replicated in part or whole for the licensed use,
-* with the restriction that this Disclaimer and Copyright notice must be
-* included with each copy of this software, whether used in part or whole,
-* at all times.
-*/
-/******************************************************************************/
-/** \file main.c
- **
- ** A detailed description is available at
- ** @link Sample Group Some description @endlink
- **
- **   - 2016-02-16  1.0  XYZ First version for Device Driver Library of Module.
- **
- ******************************************************************************/
-
-/******************************************************************************
- * Include files
- ******************************************************************************/
 #include "ddl.h"
 #include "bt.h"
 #include "lpm.h"
 #include "gpio.h"
 
-/******************************************************************************
- * Local pre-processor symbols/macros ('#define')
- ******************************************************************************/
-
-/******************************************************************************
- * Global variable definitions (declared in header file with 'extern')
- ******************************************************************************/
-
-/*******************************************************************************
- * Local variable definitions ('static')
- ******************************************************************************/
 static volatile uint32_t u32BtTestFlag = 0;
 static volatile uint32_t u32Cnt = 0;
 
-/*******************************************************************************
- * Local function prototypes ('static')
- ******************************************************************************/
-
-
-/*******************************************************************************
- * Function implementation - global ('extern') and local ('static')
- ******************************************************************************/
-
-/*******************************************************************************
- * BT1中断服务函数
- ******************************************************************************/
+/******************BT1中断服务函数********************/
 void Bt1Int(void)
 {
     if (TRUE == Bt_GetIntFlag(TIM0))
@@ -92,9 +16,7 @@ void Bt1Int(void)
     }
 }
 
-/*******************************************************************************
- * BT定时功能测试 （重载模式）
- ******************************************************************************/
+/******************BT定时功能测试（重载模式）**********************/
 en_result_t BtTimerTest(void)
 {
     stc_bt_config_t   stcConfig;
@@ -107,13 +29,12 @@ en_result_t BtTimerTest(void)
 	Gpio_SetFunc_TIM0_GATE_P35();
 //    Gpio_SetFunc_TIM1_GATE_P25();
 //    Gpio_SetFunc_TIM2_GATE_P02();
-	Gpio_SetFunc_TIM0_GATE_P35();
-    stcConfig.enGateP = BtPositive;
-    stcConfig.enGate  = BtGateEnable;
-    stcConfig.enPRS   = BtPCLKDiv16;
-    stcConfig.enTog   = BtTogDisable;
-    stcConfig.enCT    = BtTimer;
-    stcConfig.enMD    = BtMode2;
+    stcConfig.enGateP = BtPositive;		//门控极性：高电平有效
+    stcConfig.enGate  = BtGateEnable;	//门控使能：有效
+    stcConfig.enPRS   = BtPCLKDiv16;	//预分频系数：16分频
+    stcConfig.enTog   = BtTogDisable;	//反转输出使能：无效
+    stcConfig.enCT    = BtTimer;		//定时/计数功能选择：定时
+    stcConfig.enMD    = BtMode2;		//计数模式配置：自动重装载16位计数器/定时器
     //Bt初始化
     if (Ok != Bt_Init(TIM0, &stcConfig))
     {
