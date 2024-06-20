4125898c864be6cc2e6d6d8525180ade075809ce
Quinn-233 <101984896+Quinn-233@users.noreply.github.com>
2024-06-20 11:00:43 +0800

    Delete HC32F005/LS9073 directory

--- HC32F005/LS9073/project/mdk/output/malloc.d
+++
@@ -1,3 +0,0 @@
-.\output\malloc.o: ..\bsp\malloc.c
-.\output\malloc.o: ..\bsp\malloc.h
-.\output\malloc.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
--- HC32F005/LS9073/project/mdk/output/startup_hc32f005.d
+++
@@ -1,1 +0,0 @@
-.\output\startup_hc32f005.o: startup_hc32f005.s
