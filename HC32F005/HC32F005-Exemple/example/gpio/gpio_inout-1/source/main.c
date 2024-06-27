054ddca1797e5ce753438506d1bfb606a31b4c1b
Lzy <lizongyu@sys-in.com.cn>
2024-06-27 17:22:44 +0800

    更改IO口为自用电路板的KEY-IO口、LED口

--- HC32F005/HC32F005-Exemple/example/gpio/gpio_inout-1/source/main.c
+++ HC32F005/HC32F005-Exemple/example/gpio/gpio_inout-1/source/main.c
@@ -15,22 +15,23 @@ int32_t main(void)
 #if 1
     //GPIO输入
     //配置P33为输入，P33外接SW2按键
-    Gpio_InitIO(0, 1, GpioDirIn);
+    Gpio_InitIO(3, 4, GpioDirIn);
     //如果按键SW2按下，为低电平，则跳出循环继续执行程序
-    while (TRUE == Gpio_GetIO(0,1));   
+    while (TRUE == Gpio_GetIO(3,4));   
    
     //GPIO输出
     //初始化外部GPIO P03为输出、上拉、开漏，P03端口外接LED3
-    Gpio_InitIOExt(3, 5, GpioDirOut, TRUE, FALSE, TRUE, FALSE);
+    Gpio_InitIO(3, 2, GpioDirOut);
+	Gpio_SetIO(3, 2, FALSE);
     //设置GPIO值（翻转）
     while (1)
     {
-        //输出高电平，LED3灭
-        Gpio_SetIO(3, 5, TRUE);
+        //输出高电平，LED3亮
+        Gpio_SetIO(3, 2, TRUE);
         delay1ms(1000);
 
-        //输出低电平，LED3亮
-        Gpio_SetIO(3, 5, FALSE);
+        //输出低电平，LED3灭
+        Gpio_SetIO(3, 2, FALSE);
         delay1ms(1000);
     }
 #endif
