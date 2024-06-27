a715d6486352f121f3cc663b6ee930bbfdba477e
Lzy <lizongyu@sys-in.com.cn>
2024-06-27 17:24:09 +0800

    采用UART1，引脚为P23、P24，接收中断可用

--- HC32F005/HC32F005-Exemple/example/uart/uart_int-1/source/main.c
+++ HC32F005/HC32F005-Exemple/example/uart/uart_int-1/source/main.c
@@ -4,21 +4,24 @@
 #include "lpm.h"
 #include "gpio.h"
 
-#define     T1_PORT                 (3)
-#define     T1_PIN                  (3)
+uint16_t timer=0;
+uint32_t pclk=0;
 
 uint8_t u8RxData[10];
-uint8_t u8Data[]="RusHHH!\n";
-uint8_t u8Buff[10]="";
+uint8_t u8RxData1[2]={0x55,0x00};
+uint8_t u8String[]="RusHHH!\n";
+uint8_t u8Buff[100]="";
 
 uint8_t step0[]="0\n";
 uint8_t step1[]="1\n";
 uint8_t step2[]="2\n";
 uint8_t step3[]="3\n";
 
+uint8_t i = 0;
 uint8_t u8RxCnt=0; 
 uint8_t u8RxFlg=0;
 uint8_t CheckFlg=0;
+uint8_t num=0;
 
 /****************** 发送一个字节 **********************/
 void Uart_SendByte(uint8_t u8Idx,uint8_t u8Data)
@@ -37,36 +40,34 @@ void Uart_SendString(uint8_t u8Idx,uint8_t *str)
 		k++;
 	}while(*(str+k)!='\0');//直至遇到字符串结束符 '\0'
 }
-/************************接收中断回调函数************************/
+/************ 接收中断回调函数 ************/
 void RxIntCallback(void)
 {
-	u8RxData[1]=M0P_UART0->SBUF;
-	u8RxFlg = 1;
-	Uart_SendString(UARTCH0,step0);
-//	//先存储在数组中
-//	Uart_ClrStatus(UARTCH0, UartRxFull);        //清掉接收中断标志位 置0，为下次继续接收做准备
-//	u8RxData[u8RxCnt] = Uart_ReceiveData(UARTCH0);   	//接收数据字节，把缓存寄存器的值存储在数组中
-//	Uart_SetTb8(UARTCH0,Even,u8RxData[u8RxCnt]);	//
-//	Uart_SendData(UARTCH0,u8RxData[u8RxCnt]);		//接收回显
-//	u8RxCnt++;			//依次存储
-//	
-//	//u8RxCnt=0;//置0，方便下次重复以上操作
-//	if(Uart_GetStatus(UARTCH0,UartRxFull))  // TC 发送完成为真，发送完成一个字节
-//	{
-//		Uart_ClrStatus(UARTCH0,UartRxFull); //清掉发送中断标志位。置0
-//		u8RxCnt=0;//置0，方便下次重复以上操作
-//	}
+//	u8RxData[1]=M0P_UART1->SBUF;
+//	u8Buff[u8RxCnt] = Uart_ReceiveData(UARTCH1);
+	u8Buff[u8RxCnt] = M0P_UART1->SBUF;
+	if(Uart_CheckEvenOrOdd(UARTCH1,Even,u8RxData[u8RxCnt])!=Ok)
+	{
+		CheckFlg = 1;//奇偶校验出错
+	}
+	u8RxCnt++;
+//	Uart_SendByte(UARTCH1,Uart_ReceiveData(UARTCH1));		//接收回显	
+	if(u8Buff[u8RxCnt-1] == '#')
+	{
+		u8Buff[u8RxCnt-1] = '\0';
+		u8RxCnt = 0;
+		u8RxFlg = 1;
+//		Uart_SendString(UARTCH1,u8Buff);
+	}	
 }
-/************************错误中断回调函数************************/
+/************ 错误中断回调函数 ************/
 void ErrIntCallback(void)
 { 
+	Uart_SendString(UARTCH1,"Error!\n");
 }
-
-
-int32_t main(void)
-{  
-    uint16_t timer=0;
-    uint32_t pclk=0;
+/************* UART初始化函数 *************/
+void UART_Config(uint16_t uart_baund)
+{ 
 	/************************结构体重命名************************/
     stc_uart_config_t  stcConfig;
     stc_uart_irq_cb_t stcUartIrqCb;
@@ -78,19 +79,15 @@ int32_t main(void)
     DDL_ZERO_STRUCT(stcMulti);
     DDL_ZERO_STRUCT(stcBaud);
     DDL_ZERO_STRUCT(stcBtConfig);
-	/************************.************************/
-//    Gpio_InitIO(T1_PORT,T1_PIN,GpioDirIn); 
-//    Gpio_InitIO(0,3,GpioDirOut);
-//    Gpio_SetIO(0,3,1);
     /***********************TXD、RXD引脚定义************************/
-    Gpio_InitIOExt(1,4,GpioDirOut,TRUE,FALSE,FALSE,FALSE);   
-    Gpio_InitIOExt(1,5,GpioDirIn,TRUE,FALSE,FALSE,FALSE); 
+	Gpio_InitIO(2, 3, GpioDirOut);
+	Gpio_InitIO(2, 4, GpioDirOut);
 	
-    Gpio_SetFunc_UART0TX_P14();
-    Gpio_SetFunc_UART0RX_P15();
+    Gpio_SetFunc_UART1TX_P23();
+    Gpio_SetFunc_UART1RX_P24();
 	/*************************外部时钟使能**************************/
     Clk_SetPeripheralGate(ClkPeripheralBt,TRUE);	//模式0/2可以不使能
-    Clk_SetPeripheralGate(ClkPeripheralUart0,TRUE);	//
+    Clk_SetPeripheralGate(ClkPeripheralUart1,TRUE);	//
 	/************************配置中断相关内容************************/
     stcUartIrqCb.pfnRxIrqCb = RxIntCallback;		//接收中断回调函数（写了，有）
     stcUartIrqCb.pfnTxIrqCb = NULL;					//发送中断回调函数（没写）
@@ -103,57 +100,68 @@ int32_t main(void)
     stcConfig.pstcMultiMode = &stcMulti;	//配置正常或多机工作模式
 	/**************************配置波特率**************************/
     stcBaud.bDbaud = 0u;							//双倍波特率功能
-    stcBaud.u32Baud = 9600u;						//设置目标波特率
+    stcBaud.u32Baud = uart_baund;					//设置目标波特率
     stcBaud.u8Mode = UartMode3; 					//计算波特率需要模式参数
     pclk = Clk_GetPClkFreq();						//获得PCLK（4MHz）
-    timer=Uart_SetBaudRate(UARTCH0,pclk,&stcBaud);	//计算波特率所需TIMER值
+    timer=Uart_SetBaudRate(UARTCH1,pclk,&stcBaud);	//计算波特率所需TIMER值
 
     stcBtConfig.enMD = BtMode2;		//自动重装载16位计数器/定时器
     stcBtConfig.enCT = BtTimer;		//定时模式
-    Bt_Init(TIM0, &stcBtConfig);	//调用basetimer1设置函数产生波特率
-    Bt_ARRSet(TIM0,timer);			//配置重载值ARR
-    Bt_Cnt16Set(TIM0,timer);		//配置计数值CNT
-    Bt_Run(TIM0);					//启动定时
+    Bt_Init(TIM1, &stcBtConfig);	//调用basetimer1设置函数产生波特率
+    Bt_ARRSet(TIM1,timer);			//配置重载值ARR
+    Bt_Cnt16Set(TIM1,timer);		//配置计数值CNT
+    Bt_Run(TIM1);					//启动定时
 	/***********************串口及接收中断配置***********************/
-    Uart_Init(UARTCH0, &stcConfig);		//串口0初始化
-    Uart_EnableIrq(UARTCH0,UartRxIrq);	//允许串口0接收中断
-    Uart_ClrStatus(UARTCH0,UartRxFull);	//清串口0接收中断标志
-    Uart_EnableFunc(UARTCH0,UartRx);	//串口0接收中断使能
+    Uart_Init(UARTCH1, &stcConfig);		//串口1初始化
+    Uart_EnableIrq(UARTCH1,UartRxIrq);	//允许串口1接收中断
+    Uart_ClrStatus(UARTCH1,UartRxFull);	//清串口1接收中断标志
+    Uart_EnableFunc(UARTCH1,UartRx);	//串口1接收中断使能
+}
+
+int32_t main(void)
+{  
+    UART_Config(2400u);
 	
     while(1)
 	{
-		CheckFlg = 0;
-		//Uart_SendString(UARTCH0,step0);
 		if(u8RxFlg)
 		{
 			u8RxFlg = 0;
-			Uart_SendString(UARTCH0,step1);
-			if(Uart_CheckEvenOrOdd(UARTCH0,Even,u8RxData[1])!=Ok)
+//			for(i = 0; i < sizeof(u8RxData); i++)
+//			{
+//				if(Uart_CheckEvenOrOdd(UARTCH1,Even,u8RxData[i])!=Ok)
+//				{
+//					CheckFlg = 1;//奇偶校验出错
+//					break;
+//				}
+//			}	
+			if(CheckFlg != 1)
 			{
-				CheckFlg = 1;//奇偶校验出错
-				Uart_SendString(UARTCH0,step2);
-			}
-			else
-			{	
-				Uart_SendString(UARTCH0,step3);
+				Uart_SendString(UARTCH1,u8Buff);
+//				Uart_SendString(UARTCH0,step3);
+//				Uart_SetTb8(UARTCH1,Even,u8RxData[0]);
+//				Uart_SendData(UARTCH1,u8RxData[0]);				
+//				Uart_SetTb8(UARTCH1,Even,u8RxData[1]);
+//				Uart_SendData(UARTCH1,u8RxData[1]);
 			}
 		}
+
+		sprintf(u8Buff, "CheckFlg：%d\n", CheckFlg);
+		Uart_SendString(UARTCH1,u8Buff);
+		delay1ms(500);
 		
-//		sprintf(u8Buff, "%u\n", pclk);
-//		Uart_SendString(UARTCH0,u8Buff);
-		
-//		Uart_SetTb8(UARTCH0,Even,u8RxData[0]);
-//		Uart_SendData(UARTCH0,u8RxData[0]);
-//		delay1ms(500);
-//		
-//		Uart_SendByte(UARTCH0,u8RxData[1]);
+//		Uart_SetTb8(UARTCH1,Even,u8RxData[0]);
+//		Uart_SendData(UARTCH1,u8RxData[0]);
 //		delay1ms(500);
 		
-//		Uart_SendString(UARTCH0,u8Data);
+//		Uart_SendByte(UARTCH1,u8String[0]);
 //		delay1ms(500);
 		
-//		sprintf(u8Buff, "%u\n", pclk);
-//		Uart_SendString(UARTCH0,u8Buff);
+//		sprintf(u8Buff, "CLK：%d\n", pclk);
+//		Uart_SendString(UARTCH1,u8Buff);
+//		delay1ms(500);
+		
+//		Uart_SendString(UARTCH1,u8String);
 //		delay1ms(500);
 	}
 }
